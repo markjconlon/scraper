7269b54ab941220b248b0de58b31c305942418eb
markjconlon <markjconlon@gmail.com>
2017-08-15 20:53:03 -0400

    works with an array and block

--- mechanize.rb
+++ mechanize.rb
@@ -7,13 +7,15 @@ agent.user_agent_alias = 'Mac Safari'
 page = agent.get('https://wikipedia.org/')
 search_form = page.form(action: '//www.wikipedia.org/search-redirect.php')
 
-search_form.search = 'Steve Jobs'
+search_terms = ['Steve Jobs', 'henry ford', 'andrew carnegie', 'benjamin franklin',
+  'Thomas Edison', 'ray kroc']
 
-results = agent.submit(search_form, search_form.button('go'))
-
-html_results = Nokogiri::HTML(results.body)
-name = html_results.at_css('#firstHeading').text
-bday = html_results.at_css('.bday').text
-dday = html_results.at_css('.dday').text
-
-puts "#{name} born #{bday}, died #{dday}"
+search_terms.each do |term|
+  search_form.search = term
+  results = agent.submit(search_form, search_form.button('go'))
+  html_results = Nokogiri::HTML(results.body)
+  name = html_results.at_css('#firstHeading').text
+  bday = html_results.at_css('.bday').text
+  dday = html_results.at_css('.dday').text
+  puts "#{name} born #{bday}, died #{dday}"
+end
