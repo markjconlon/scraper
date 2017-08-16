7645edcd5f221bc9383bf39a6d32fe1c80050631
markjconlon <markjconlon@gmail.com>
2017-08-15 20:40:02 -0400

    first test of using mechanize's agent to search the page and pass it into Nokogiri to allow us to search for the name, bday, dday, and cause of death as is scraper.rb

--- mechanize.rb
+++ mechanize.rb
@@ -5,5 +5,10 @@ agent.user_agent_alias = 'Mac Safari'
 }
 
 page = agent.get('https://wikipedia.org/')
+search_form = page.form(action: '//www.wikipedia.org/search-redirect.php')
 
-pp page
+search_form.search = 'Steve Jobs'
+
+results = agent.submit(search_form, search_form.button('go'))
+
+html_results = Nokogiri::HTML(results.body)
