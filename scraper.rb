67a155d9f7192a7936a8cf689840fcc406daf586
markjconlon <markjconlon@gmail.com>
2017-08-15 20:21:51 -0400

    ensuring each selector is still working from notes

---
+++ scraper.rb
@@ -0,0 +1,13 @@
+require 'nokogiri'
+
+file = File.open('stevejobs.html')
+html = Nokogiri::HTML(file)
+
+output = File.new('stevejobs_story.txt', 'w+')
+
+name = html.at_css('#firstHeading').text
+bday = html.at_css('.bday').text
+dday = html.at_css('.dday').text
+cause_of_death = html.at_xpath('//a[@title="Pancreatic cancer"]').text
+
+puts name,bday,dday,cause_of_death
